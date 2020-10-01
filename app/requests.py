592f1edc27368aa99d1b9e07eac2b94c042ac7d8
John-Osiko <johnmaxosiko@gmail.com>
2020-10-01 15:21:06 +0300

    Renamed file

--- app/requests.py
+++
@@ -1,7 +0,0 @@
-import requests,json
-
-def get_news():
-    response = requests.get('https://newsapi.org/v2/sources?language=en&apiKey={}')
-    if response.status_code == 200:
-        news = response.json()
-        return news
