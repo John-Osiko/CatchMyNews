a511e33d17af155d7ec39c2fd398174bd0464e82
John-Osiko <johnmaxosiko@gmail.com>
2020-10-02 23:22:55 +0300

    Added sources class

--- app/models.py
+++ app/models.py
@@ -1,49 +1,27 @@
 class News:
+    """ 
+    News class to define Article Objects
+    
     """
-    News class to define news objects
-    """
+    def __init__(self, source, author, title, description, url, urlToImage, publishedAt):
 
-    def  __init__(self,id,source,author,description,urlToImage,publishedAt,content):
-        self.id = id
         self.source = source
         self.author = author
+        self.title = title
         self.description = description
-        self.urlToImage = urlToImage
+        self.url = url
+        self.urlToImage= urlToImage
         self.publishedAt = publishedAt
-        self.content = content
-
-
 
 class Sources:
-    def __init__(self,source_id,name,description,country):
-        self.source_id = source_id
+    '''
+    Sources class that defines each source object
+    '''
+    def __init__(self,id,name,description,url,category,language,country):
+        self.id = id
         self.name = name
         self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
         self.country = country
-class Review:
-    all_reviews = []
-
-    def __init__(self,news_id,description,urlToImage,review):
-        self.news_id = news_id
-        self.title = title
-        self.urlToImage = urlToImage
-        self.review = review
-
-    def save_review(self):
-        Review.all_reviews.append(self)
-
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-
-    @classmethod
-    def get_reviews(cls,id):
-
-        response = []
-
-        for review in cls.all_reviews:
-            if review.news_id == id:
-                response.append(review)
-
-        return response
