13a8c715d3a36d985c46b9801d14171b39f4b157
John-Osiko <johnmaxosiko@gmail.com>
2020-10-02 23:19:11 +0300

    Modified base and api urls

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&apiKey={}'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    NEWS_API_KEY=os.environ.get("42ff49abe2dd44e08eb28ab10b3103a6")
+    NEWS_API_BASE_URL='https://newsapi.org/v2/sources?country=us&category={}&apiKey=42ff49abe2dd44e08eb28ab10b3103a6'
+    NEWS_ARTICLES_APL_URL='https://newsapi.org/v2/everything?q={}&apiKey=42ff49abe2dd44e08eb28ab10b3103a6'
+
 
 
 
